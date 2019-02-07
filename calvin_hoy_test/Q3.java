f3e7bebcb0ea89f3257f9d58e6c44a5c4bbdacb0
hoycalvin <hoycalvin5@gmail.com>
2019-02-07 16:50:10 -0700

    Add files via upload

---
+++ calvin_hoy_test/Q3.java
@@ -0,0 +1,21 @@
+/*
+    LRU = Least recently used
+    
+
+
+*/
+
+import java.util.*;
+
+public class Q3{
+    //constructor
+    public Q3(){
+        size = 3;
+    }
+
+
+    public static void main(String[] args){ 
+        Q3 lruClass = new Q3();
+    }
+    
+}
